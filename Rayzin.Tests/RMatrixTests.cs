12b2d004e674d3bce2e171c90ddb77d54715fbea
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2023-11-12 12:47:33 +0100

    RMatrix basic operations

---
+++ Rayzin.Tests/RMatrixTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework.Interfaces;
+
+namespace Rayzin.Tests;
+
+public class RMatrixTests
+{
+    [Test]
+    public void Constructor_2x3Matrix_ThrowsArgumentOutOfRangeException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = new RMatrix(1, 2, 3, 4, 5, 6));
+    }
+
+    [Test]
+    public void Constructor_NullMatrix_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => _ = new RMatrix(null!));
+    }
+    
+    [Test]
+    public void Constructor_4x4Matrix_CreatesCorrectMatrix()
+    {
+        var matrix = new RMatrix(1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5);
+        Assert.That(matrix[0, 0], Is.EqualTo(1));
+        Assert.That(matrix[0, 3], Is.EqualTo(4));
+        Assert.That(matrix[1, 0], Is.EqualTo(5.5));
+        Assert.That(matrix[1, 2], Is.EqualTo(7.5));
+        Assert.That(matrix[2, 2], Is.EqualTo(11));
+        Assert.That(matrix[3, 0], Is.EqualTo(13.5));
+        Assert.That(matrix[3, 2], Is.EqualTo(15.5));
+    }
+    
+    [Test]
+    public void Constructor_3x3Matrix_CreatesCorrectMatrix()
+    {
+        var matrix = new RMatrix(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Assert.That(matrix[0, 0], Is.EqualTo(1));
+        Assert.That(matrix[1, 1], Is.EqualTo(5));
+        Assert.That(matrix[2, 2], Is.EqualTo(9));
+    }
+    
+    [Test]
+    public void Constructor_2x2Matrix_CreatesCorrectMatrix()
+    {
+        var matrix = new RMatrix(1, 2, 3, 4);
+        Assert.That(matrix[0, 0], Is.EqualTo(1));
+        Assert.That(matrix[1, 1], Is.EqualTo(4));
+    }
+
+    [Test]
+    [TestCase(-1, 0)]
+    [TestCase(0, -1)]
+    [TestCase(2, 0)]
+    [TestCase(0, 2)]
+    public void ReadValue_OutOfBounds_ThrowsArgumentOutOfRangeException(int x, int y)
+    {
+        var matrix = new RMatrix(1, 2, 3, 4);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = matrix[y, x]);
+    }
+}
---
+++ Rayzin/RMatrix.cs
@@ -0,0 +1,33 @@
+﻿namespace Rayzin;
+
+public readonly record struct RMatrix
+{
+    private readonly double[] _values;
+    private readonly int _size;
+
+    public RMatrix(params double[] values)
+    {
+        _values = values ?? throw new ArgumentNullException(nameof(values));
+        if (values.Length == 4)
+            _size = 2;
+        else if (values.Length == 9)
+            _size = 3;
+        else if (values.Length == 16)
+            _size = 4;
+        else
+            throw new ArgumentOutOfRangeException(nameof(values), "values must have exactly 4, 9, or 16 items");
+    }
+
+    public double this[int x, int y]
+    {
+        get
+        {
+            if (x < 0 || x >= _size)
+                throw new ArgumentOutOfRangeException(nameof(x), $"x must be in the range of 0..{_size}");
+            if (y < 0 || y >= _size)
+                throw new ArgumentOutOfRangeException(nameof(y), $"y must be in the range of 0..{_size}");
+
+            return _values[x * _size + y];
+        }
+    }
+}
