3ec46873c408b2b63639993e92add2eba5b7e077
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2023-11-12 12:27:19 +0100

    Testing canvas saving

--- Rayzin.Sandbox.Console/Program.cs
+++ Rayzin.Sandbox.Console/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Rayzin;
+
+var canvas = new RCanvas(256, 256);
+for (var y = 0; y < 256; y++)
+    for (var x = 0; x < 256; x++)
+        canvas[x, y] = new RColor(x / 255.0, y / 255.0, 128);
+        
+canvas.Save(@"D:\Temp\test.ppm", RCanvasFileFormat.PPM8);
