630b650b3269b536f05e8085b5d2b39e300c3dfa
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2023-11-12 14:14:25 +0100

    Translation transformation

---
+++ Rayzin.Tests/RTransformTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Rayzin.Tests;
+
+public class RTransformTests
+{
+    [Test]
+    public void Translate()
+    {
+        RMatrix transform = RTransform.Translate(5, -3, 2);
+        RPoint p = (-3, 4, 5);
+        Assert.That(transform * p, Is.EqualTo(new RPoint(2, 1, 7)));
+    }
+    
+    [Test]
+    public void Translate_Inverse()
+    {
+        RMatrix transform = RTransform.Translate(5, -3, 2);
+        RMatrix inv = transform.Invert();
+        RPoint p = (-3, 4, 5);
+        Assert.That(inv * p, Is.EqualTo(new RPoint(-8, 7, 3)));
+    }
+
+    [Test]
+    public void Translate_Vector()
+    {
+        RMatrix transform = RTransform.Translate(5, -3, 2);
+        RVector v = (-3, 4, 5);
+        Assert.That(transform * v, Is.EqualTo(v));
+    }
+}
---
+++ Rayzin/RTransform.cs
@@ -0,0 +1,6 @@
+﻿namespace Rayzin;
+
+public static class RTransform
+{
+    public static RMatrix Translate(int x, int y, int z) => new(1, 0, 0, x, 0, 1, 0, y, 0, 0, 1, z, 0, 0, 0, 1);
+}
