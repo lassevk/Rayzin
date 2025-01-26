32a1b73afb7c2b0e591535336dc84600ec596ac1
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-01-26 18:33:59 +0100

    RTuple addition

--- Rayzin.Tests/RTupleTests.cs
+++ Rayzin.Tests/RTupleTests.cs
@@ -8,6 +8,8 @@ public class RTupleTests
         var tuple = RTuple.Point(4.3, -4.2, 3.1);
 
         Assert.That(tuple.W, Is.EqualTo(1.0));
+        Assert.That(tuple.IsPoint, Is.True);
+        Assert.That(tuple.IsVector, Is.False);
     }
 
     [Test]
@@ -16,5 +18,49 @@ public class RTupleTests
         var tuple = RTuple.Vector(4.3, -4.2, 3.1);
 
         Assert.That(tuple.W, Is.EqualTo(0.0));
+        Assert.That(tuple.IsPoint, Is.False);
+        Assert.That(tuple.IsVector, Is.True);
+    }
+
+    [Test]
+    public void AddOperator_TwoPoints_ThrowsInvalidOperationException()
+    {
+        var p1 = RTuple.Point(3, -2, 5);
+        var p2 = RTuple.Point(-2, 3, 1);
+
+        Assert.Throws<InvalidOperationException>(() => _ = p1 + p2);
+    }
+
+    [Test]
+    public void AddOperator_TwoVectors_ReturnsVector()
+    {
+        var p1 = RTuple.Vector(3, -2, 5);
+        var p2 = RTuple.Vector(-2, 3, 1);
+
+        RTuple<int> p3 = p1 + p2;
+
+        Assert.That(p3, Is.EqualTo(RTuple.Vector(1, 1, 6)));
+    }
+
+    [Test]
+    public void AddOperator_PointAndVector_ReturnsPoint()
+    {
+        var p1 = RTuple.Point(3, -2, 5);
+        var p2 = RTuple.Vector(-2, 3, 1);
+
+        RTuple<int> p3 = p1 + p2;
+
+        Assert.That(p3, Is.EqualTo(RTuple.Point(1, 1, 6)));
+    }
+
+    [Test]
+    public void AddOperator_VectorAndPoint_ReturnsPoint()
+    {
+        var p1 = RTuple.Vector(3, -2, 5);
+        var p2 = RTuple.Point(-2, 3, 1);
+
+        RTuple<int> p3 = p1 + p2;
+
+        Assert.That(p3, Is.EqualTo(RTuple.Point(1, 1, 6)));
     }
 }
--- Rayzin/RTuple.cs
+++ Rayzin/RTuple.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using System.Security.AccessControl;
 
 namespace Rayzin;
 
@@ -13,4 +14,17 @@ public readonly record struct RTuple<T>
 
     public bool IsPoint => W == T.One;
     public bool IsVector => W == T.Zero;
+
+    public static RTuple<T> operator +(RTuple<T> left, RTuple<T> right)
+    {
+        if (left.IsPoint && right.IsPoint)
+        {
+            throw new InvalidOperationException("Cannot add two points.");
+        }
+
+        return new RTuple<T>
+        {
+            X = left.X + right.X, Y = left.Y + right.Y, Z = left.Z + right.Z, W = left.W + right.W,
+        };
+    }
 }
